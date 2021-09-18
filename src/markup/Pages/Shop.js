b609936e1f51ade62cd6fa5c9e4bae6d173ef96e
theuniquecreations <vbalakumar.cse@gmail.com>
2021-09-18 09:57:46 -0400

    sale

--- src/markup/Pages/Shop.js
+++ src/markup/Pages/Shop.js
@@ -395,18 +395,24 @@ const Shop = (props) => {
                               <div className="item-img">
                                 <img className="thumbnailimage" src={product.p_image} alt="" />
                                 {product.p_actual_price !== product.p_price ? (
-                                  <div className="price bg-white">
-                                    <span style={{ "text-decoration": "line-through" }}>
-                                      {" "}
-                                      <i class="fa fa-inr"></i> {product.p_actual_price || 0}{" "}
-                                    </span>
-                                    {"   |  "}
-                                    <span>
-                                      {"   "} <i class="fa fa-inr"></i> {product.p_price}
-                                    </span>
-                                  </div>
+                                  <>
+                                    <div className="price bg-white">
+                                      <span style={{ "text-decoration": "line-through" }}>
+                                        {" "}
+                                        <i class="fa fa-inr"></i> {product.p_actual_price || 0}{" "}
+                                      </span>
+                                      {"   |  "}
+                                      <span>
+                                        {"   "} <i class="fa fa-inr"></i> {product.p_price}
+                                      </span>
+                                    </div>
+                                    <div className="sale bg-primary text-light">Sale</div>
+                                  </>
                                 ) : (
-                                  <div className="price bg-white">Rs.{product.p_price}</div>
+                                  <div className="price bg-white">
+                                    <i class="fa fa-inr"> {"   "} </i>
+                                    {"   "} {product.p_price}
+                                  </div>
                                 )}
                               </div>
                               <div className="item-info text-center">
