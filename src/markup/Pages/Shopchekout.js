2bf27b217fb6bef8a597b68310f91842a2946b17
theuniquecreations <vbalakumar.cse@gmail.com>
2021-12-17 12:20:25 -0500

    fix

--- src/markup/Pages/Shopchekout.js
+++ src/markup/Pages/Shopchekout.js
@@ -411,7 +411,7 @@ const Shopchekout = () => {
                         </tbody>
                       </table>
                       <h4>Payment Method</h4>
-                      <h6>Contact Owner</h6>
+
                       <div className="d-none">
                         <div className="form-group">
                           <input type="text" className="form-control" placeholder="Name on Card" />
